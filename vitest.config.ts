54e3dfde1575bcb5b0df8e6189d2844d79589031
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2025-07-30 14:10:32 +0200

    chore: Fixed code coverage stats

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ export default defineConfig({
 	test: {
 		exclude: ['**/node_modules/**', '**/dist/**'],
 		testTimeout: 20000,
+		coverage: {
+			provider: 'v8',
+			include: ['src/**'],
+		},
 	},
 	esbuild: {
 		target: 'node20',
