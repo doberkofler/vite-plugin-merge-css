1f72522d8fa0c6675cd68ee77fcaa7655b82303b
Dieter Oberkofler <1009585+doberkofler@users.noreply.github.com>
2025-07-30 13:57:27 +0200

    chore: Add more comprehensive test of plugin

---
+++ src/__tests__/plugin.spec.ts
@@ -0,0 +1,88 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import os from 'node:os';
+//import {fileURLToPath} from 'node:url';
+import {describe, it, expect} from 'vitest';
+import VitePluginMergeCss from '../index';
+import {build} from 'vite';
+
+const expectFileToContain = async (outDir: string, filename: string, content: string | string[]): Promise<void> => {
+	const output = await fs.readFile(path.join(outDir, filename), 'utf-8');
+
+	if (Array.isArray(content)) {
+		for (const i of content) {
+			expect(output).toContain(i);
+		}
+	} else {
+		expect(output).toContain(content);
+	}
+};
+
+describe('VitePluginMergeCss', () => {
+	it('transforms code that does not need the plugin', async () => {
+		const tempDir = os.tmpdir();
+
+		const outDir = path.join(tempDir, 'vite-plugin-merge-css', 'dist');
+		await fs.mkdir(outDir, {recursive: true});
+
+		const srcDir = path.join(tempDir, 'vite-plugin-merge-css', 'src');
+		await fs.mkdir(srcDir, {recursive: true});
+
+		const inputFile = path.resolve(path.join(srcDir, 'input.ts'));
+		await fs.writeFile(inputFile, 'console.log("test");');
+
+		await build({
+			logLevel: 'silent',
+			build: {
+				manifest: true,
+				rollupOptions: {
+					input: inputFile,
+					output: {
+						entryFileNames: '[name].js',
+					},
+				},
+				cssCodeSplit: true,
+				outDir,
+				emptyOutDir: true,
+				minify: false,
+			},
+			plugins: [VitePluginMergeCss()],
+		});
+
+		await expectFileToContain(outDir, 'input.js', 'console.log("test");');
+	});
+
+	it('transforms code from example', async () => {
+		const tempDir = os.tmpdir();
+
+		const outDir = path.join(tempDir, 'vite-plugin-merge-css', 'dist');
+		await fs.mkdir(outDir, {recursive: true});
+
+		await build({
+			logLevel: 'silent',
+			build: {
+				manifest: true,
+				rollupOptions: {
+					input: {
+						page_a: path.resolve('example/src/page_a/index.ts'),
+						page_b: path.resolve('example/src/page_b/index.ts'),
+					},
+					output: {
+						entryFileNames: '[name].js',
+					},
+				},
+				cssCodeSplit: true,
+				outDir,
+				emptyOutDir: true,
+				minify: false,
+			},
+			plugins: [VitePluginMergeCss()],
+		});
+
+		await expectFileToContain(outDir, 'page_a.js', 'document.getElementById');
+		await expectFileToContain(outDir, 'page_a.css', ['background: white; /* white background */', 'color: lightgray; /* clock has lightgrey color */']);
+
+		await expectFileToContain(outDir, 'page_b.js', 'document.getElementById');
+		await expectFileToContain(outDir, 'page_b.css', ['background: grey; /* grey background */', 'color: lightgray; /* clock has lightgrey color */']);
+	});
+});
